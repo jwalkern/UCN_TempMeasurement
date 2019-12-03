bde6294eb759eb21182c1ad22f6fa396b0a7e4d4
jwalkern <56714873+jwalkern@users.noreply.github.com>
2019-12-03 10:55:06 +0100

    OCD riastian

--- TempSensor.py
+++ TempSensor.py
@@ -1,7 +1,7 @@
 import time
 from gpiozero import MCP3008
 
-for i in range(10):
-    data = MCP3008(port=4, device=1)
+for i in range(0):
+    data = MCP3008(channel=4, device=1)
     print(data)
     time.sleep(0.1)
