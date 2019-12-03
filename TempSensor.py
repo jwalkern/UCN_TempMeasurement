950411d21e8ced333238c39c0459e013acd8ff11
jwalkern <56714873+jwalkern@users.noreply.github.com>
2019-12-03 10:47:15 +0100

    OCD riastian

--- TempSensor.py
+++ TempSensor.py
@@ -1,16 +1,7 @@
-import RPi.GPIO as GPIO
 import time
 from gpiozero import MCP3008
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(21, GPIO.IN)
-
-promt = ""
 for i in range(10):
     data = MCP3008(channel=4,device=1)
     print(data)
     time.sleep(0.1)
-GPIO.cleanup()
-
-
-
