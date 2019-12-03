8474305777fdeaddd6a38d34b0066b813f576b81
jwalkern <56714873+jwalkern@users.noreply.github.com>
2019-12-03 10:45:10 +0100

    ut32it8

--- TempSensor.py
+++ TempSensor.py
@@ -3,11 +3,11 @@ import time
 from gpiozero import MCP3008
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(21, GPIO.OUT)
+GPIO.setup(21, GPIO.IN)
 
 promt = ""
 for i in range(10):
-    data = GPIO.input(21)
+    data = MCP3008(channel=4,device=1)
     print(data)
     time.sleep(0.1)
 GPIO.cleanup()
