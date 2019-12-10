39138997a217d6d765ee1d897ebee425940fba7f
jwalkern <56714873+jwalkern@users.noreply.github.com>
2019-12-10 11:49:25 +0100

    tester logging funktion

--- SPITemp2.py
+++ SPITemp2.py
@@ -49,8 +49,8 @@ try:
 
 
         #Tester de nye logging funktioner
-        templogging(temp, temp2)
-        jsonlogging(templogging(temp, temp2))
+        templogging(temp2, temp)
+        jsonlogging(templogging(temp2, temp))
 
         sleep(5)
 
